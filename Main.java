6de4f7a81a4dddd9d3601971c50d77169bbbf86f
David Elbakian <elbakiand@gmail.com>
2021-07-24 11:52:34 +0400

    javasyntaxpro8l3

--- Main.java
+++ Main.java
@@ -18,6 +18,13 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /* 1
+        Часто перед началом использования массива его необходимо заполнить значениями по умолчанию.
+        Реализуй такое заполнение в методе fillArray(Integer[] array, int value).
+        В качестве аргументов метод принимает массив и значение, которым его необходимо заполнить.
+        Массив заполняется полностью, независимо от его длины.        
+        */
+        
         Integer[] array = new Integer[5];
         fillArray(array, 3);
         System.out.println(Arrays.toString(array));
