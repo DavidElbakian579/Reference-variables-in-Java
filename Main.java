4e6bbb48a0a0a0ab8934815f44d87d467f5e63bb
David Elbakian <elbakiand@gmail.com>
2021-07-24 11:55:53 +0400

    Reference variables in Java

--- Main.java
+++ Main.java
@@ -28,10 +28,29 @@ public class Main {
         Integer[] array = new Integer[5];
         fillArray(array, 3);
         System.out.println(Arrays.toString(array));
+        
+        /* 2
+        Иногда необходимо заполнить какую-то часть массива одинаковыми значениями или же удалить существующие (присвоить значение null).
+        Реализуй для этого более гибкий метод fillArray(Integer[] array, int value, int begin, int end), где:
+        array — массив, который необходимо заполнить;
+        value — значение, которым необходимо заполнить;
+        begin — начиная с какого индекса необходимо заполнять (включительно);
+        end — на каком индексе остановить заполнение (исключая).
+        */
+        
+        Integer[] array2 = new Integer[5];
+        fillArray2(array2, 3, 1, 3);
+        System.out.println(Arrays.toString(array));
     }
     
     public static void fillArray(Integer[] array, int value){
         for (int i = 0; i < array.length; i++) {
+            array[i] = value;
+        }
+    }
+    
+    public static void fillArray2(Integer[] array, int value, int begin, int end) {
+        for (int i = begin; i < end; i++) {
             array[i] = value;
         }
     }
